ad2762762b0cfa5e32187c5c353eb46a0f915b59
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-15 23:41:23 -0300

    Entendendo o JSX e className

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  const nome = "Daniel";
+  const url = 'https:via.placeholder.com/150'
+
+  function subtracao(num1, num2){
+    return num1 - num2
+  }
+
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <p>Meu primeiro App!</p>
+      <h1>JSX</h1>
+      <p>Olá {nome}!</p>
+      <p>Soma = 2 + 2 = {2 + 2}</p>
+      <p>Subtração = 10 - 5 = {subtracao(10, 5)}</p>
+      <img src={url} alt="Minha imagem" />
     </div>
   );
 }
